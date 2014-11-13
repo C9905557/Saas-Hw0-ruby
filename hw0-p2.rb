16c7d80d8a33b3e657ec0fcf09792c79e0b35e9e
Jean-Pierre Bernard <jeanpierre.bernard@gmail.com>
2014-11-13 01:38:01 +0000

    Simplification to binary_multiple_of_4? and alternate algorithm for starts_with_consonant

--- hw0-p2.rb
+++ hw0-p2.rb
@@ -10,7 +10,10 @@ end
 # (For our purposes, a consonant is any letter other than A, E, I, O, U.)
 # NOTE: be sure it works for both upper and lower case and for nonletters!
 def starts_with_consonant?(s)
-  s =~ /^[^aeiou]/i && s =~ /^[a-z]/i
+  #s =~ /^[^aeiou]/i && s =~ /^[a-z]/i
+  # Alternate version with string index; 
+  # .index returns a number if the char is found == true; returns nil == false otherwise
+  "bcdfghjklmnpqrstvwxyz".index(s[0].downcase)
 end
 
 
@@ -18,7 +21,7 @@ end
 # and returns true if the string represents a binary number that is a multiple of 4.
 # NOTE: be sure it returns false if the string is not a valid binary number!
 def binary_multiple_of_4?(s)
-    return (s == "0" or s =~ /^[01]*00$/)
+    s =~ /(^[01]*00$)|^0$/
 end
 
 def test_hello
@@ -34,19 +37,21 @@ def test_starts_with_consonant
     raise "Starts with a" if starts_with_consonant?('asdfsdf-')
     raise "Starts with U" if starts_with_consonant?('Uasdfsdf-')
     raise "Starts with d" unless starts_with_consonant?('dasdfsdf-')
+    raise "Starts with D" unless starts_with_consonant?('Ddasdfsdf-')
     puts 'test_starts_with_consonant passed'
 end
 
 test_starts_with_consonant
 
 def test_binary_multiple_of_4
-    raise "000 should be multple of 4" unless binary_multiple_of_4?("000")
-    raise "0 should be multple of 4" unless binary_multiple_of_4?("0")
-    raise "1111110001 should be multple of 4" unless binary_multiple_of_4?("10100")
-    raise "1111110001 should not be multple of 4" if binary_multiple_of_4?("1111110001")
-    raise "111111111 should not be multple of 4" if binary_multiple_of_4?("111111111")
-    raise "001100110011 should not be multple of 4" if binary_multiple_of_4?("001100110011")
-    raise "aa should not be multple of 4" if binary_multiple_of_4?("aa")
+    raise "000 should be multiple of 4" unless binary_multiple_of_4?("000")
+    raise "0 should be multiple of 4" unless binary_multiple_of_4?("0")
+    raise "10100 should be multiple of 4" unless binary_multiple_of_4?("10100")
+    raise "1111110001 should not be multiple of 4" if binary_multiple_of_4?("1111110001")
+    raise "111111111 should not be multiple of 4" if binary_multiple_of_4?("111111111")
+    raise "001100110011 should not be multiple of 4" if binary_multiple_of_4?("001100110011")
+    raise "12 should not be multiple of 4 (binary wise)" if binary_multiple_of_4?("12")
+    raise "aa should not be multiple of 4" if binary_multiple_of_4?("aa")
     puts "test_binary_multiple_of_4 passed"
 end
 
